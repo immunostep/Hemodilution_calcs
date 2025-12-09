998234e0a635c503a875c6fa57b0003f1cb9e41b
Adrian Lecrevisse <alecrevisse@immunostep.com>
2025-12-09 09:02:42 +0100

    v2.0.1

--- hemodilution_calcs/main.py
+++ hemodilution_calcs/main.py
@@ -1,16 +1,16 @@
 from ._operations import load_fcs, getImf, custom_hdbscan, apply_threshold, gate_diagonal_percentile_df, setChannels
 from typing import Tuple
 
-def getHemodilutionEventsImf(fcsFilePath: str) -> Tuple[int, float]:
+def getHemodilutionEventsImf(fcsFilePath: str, plot = False) -> Tuple[int, float]:
     meta, df = load_fcs(fcsFilePath)
     cit = setChannels(meta)
 
     if cit not in ["calibur", "FACSCalibur"]:
-        df_diagonal = gate_diagonal_percentile_df(df, plot=False)
+        df_diagonal = gate_diagonal_percentile_df(df, plot=plot)
     else:
         df_diagonal = df.copy()
-    df_threshold = apply_threshold(df_diagonal, plot=False)
-    final = custom_hdbscan(df_threshold, plot=False)
+    df_threshold = apply_threshold(df_diagonal, plot=plot)
+    final = custom_hdbscan(df_threshold, plot=plot)
 
     event_count = final.shape[0]
     imf = getImf(final)
